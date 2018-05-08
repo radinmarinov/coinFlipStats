c2ed5c428176beb66f03dfbd2c630a8af6fe07bd
Nikhil Patel <Nikhil@nikhils-macbook-pro.local>
2018-05-08 18:47:53 -0500

    Initial Commit

---
+++ pricegetterTests/pricegetterTests.swift
@@ -0,0 +1,36 @@
+//
+//  pricegetterTests.swift
+//  pricegetterTests
+//
+//  Created by Nikhil Patel on 5/8/18.
+//  Copyright © 2018 Nikhil Patel. All rights reserved.
+//
+
+import XCTest
+@testable import pricegetter
+
+class pricegetterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
